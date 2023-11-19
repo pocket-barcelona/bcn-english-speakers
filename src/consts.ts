8443db3c8ee5adbe26e38747771e3d8e8f9d3199
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-11-19 17:54:50 +0100

    Tidyup old filer and tailwind prose

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Pocket Barcelona';
+export const SITE_DESCRIPTION = 'Experience Barcelona like a local';
