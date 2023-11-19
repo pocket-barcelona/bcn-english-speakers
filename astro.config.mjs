2e48d86bb466868fe4b49f514d21b7a3dd11d397
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-11-19 18:10:13 +0100

    Added Astro image tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from "@astrojs/cloudflare";
-
+import { astroImageTools } from "astro-imagetools";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), astroImageTools],
   output: "server",
   adapter: cloudflare()
 });
