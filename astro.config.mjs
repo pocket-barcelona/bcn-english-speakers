1c106107fe8e12e7d6aa1e9b0f81baa31a23e36b
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-11-20 12:00:41 +0100

    Removed Astro imagetools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import cloudflare from "@astrojs/cloudflare";
-import { astroImageTools } from "astro-imagetools";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), astroImageTools],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  image: {
+    service: passthroughImageService()
+  }
 });
