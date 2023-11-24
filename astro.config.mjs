7ba501ecf61d80003f0e885b71e7993739a3a957
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-11-24 18:35:08 +0100

    Fixing build and enabled static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.pocketbarcelona.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server"
+  output: "static", // https://docs.astro.build/en/guides/server-side-rendering/
+  // adapter: node({
+  //   mode: "standalone"
+  // }),
 });
