ab47e39d290ed92387cb521c8ba7d306b9e1dc99
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-11-24 17:16:19 +0100

    Setup Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.pocketbarcelona.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: cloudflare(),
-  image: {
-    service: passthroughImageService()
-  }
+  output: "server"
 });
