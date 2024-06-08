57bc074df92d8d2e570e2c614f4d199a84a2a6e5
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2024-06-08 19:31:35 +0200

    Groups come direct from Directus

--- src/services/group.service.ts
+++ src/services/group.service.ts
@@ -1,68 +1,44 @@
-import {type CollectionEntry} from 'astro:content';
-import {HEADLESS_STUB} from '../consts';
-import {readItems} from "@directus/sdk";
-import directus, {type HeadlessGroup} from '../lib/directus';
+import { HEADLESS_STUB } from "../consts";
+import { readItems } from "@directus/sdk";
+import directus, { type HeadlessGroup } from "../lib/directus";
 
 export async function getGroups() {
-  return (await getTransformedHeadlessGroups())
-  .sort((a, b) => {
-    return b.data.id < a.data.id ? 1 : -1;
+  return (await getTransformedHeadlessGroups()).sort((a, b) => {
+    return b.id < a.id ? 1 : -1; // might not need this if doing sort in directus request
   });
 }
 
-/** Transform a Directus post into our blog post type */
-async function getTransformedHeadlessGroups(): Promise<CollectionEntry<'group'>[]> {
+/** Fetch Whatsapp Groups from Directus */
+async function getTransformedHeadlessGroups(): Promise<
+  HeadlessGroup[]
+> {
   const groups = await directus.request(
-      readItems("groups", {
-        fields: [
-          "icon",
-          "slug",
-          "link",
-          "name",
-          "description",
-          "id",
-        ],
-        sort: ["-id"],
-      })
+    readItems("groups", {
+      fields: ["icon", "slug", "link", "name", "description", "id"],
+      sort: ["-id"],
+    })
   );
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  return transformHeadlessGroups(groups as any);
+  return groups;
 }
 
-/** Transform REST response to Astro Collection */
-function transformHeadlessGroups(groups: HeadlessGroup[]): CollectionEntry<'group'>[] {
-  return groups.map(p => {
-    return {
-      id: p.slug,
-      slug: p.slug,
-      collection: 'groups',
-      data: {
-        id: p.id,
-        name: p.name,
-        description: p.description,
-        link: p.link,
-        icon: `${HEADLESS_STUB}/assets/${p.icon}`,
-        __source: 'HEADLESS' // inject identifier, if needed explicitly
-      }
-    }
-  }) as unknown as CollectionEntry<'group'>[];
-}
-
-
-async function getHeadlessGroupById(slug: string): Promise<{ data: HeadlessGroup; }> {
+async function getHeadlessGroupById(
+  slug: string
+): Promise<{ data: HeadlessGroup }> {
   const endpoint = `${HEADLESS_STUB}/items/groups/${slug}`;
   const resp = await fetch(endpoint);
   const data = await resp.json();
-  return data as Promise<{ data: HeadlessGroup; }>;
+  return data as Promise<{ data: HeadlessGroup }>;
 }
 
-export async function getGroup(slug: string | undefined): Promise<{ data: HeadlessGroup | null }> {
-  if (!slug) return {data: null};
+export async function getGroup(
+  slug: string | undefined
+): Promise<{ data: HeadlessGroup | null }> {
+  if (!slug) return { data: null };
 
   try {
     return await getHeadlessGroupById(slug);
   } catch (e) {
     console.error(e);
-    return {data: null};
+    return { data: null };
   }
 }
