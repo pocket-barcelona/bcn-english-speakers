ef33ab338032578ec69a31539c8abad0e3f0e26a
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2024-10-21 19:19:26 +0200

    Setting up fetch service

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = 'Pocket Barcelona';
 export const SITE_DESCRIPTION = 'Experience Barcelona like a local';
 export const HEADLESS_STUB = 'https://content.pocketbarcelona.com';
+export const API_STUB = 'https://api.pocketbarcelona.com';
