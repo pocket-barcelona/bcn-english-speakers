58b918a14ef82c1b2c71faeb3fb71ed4b95c78e4
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2024-10-22 09:46:24 +0200

    Fetch meetup data from API

--- src/components/events/screens/EventsList/EventsList.tsx
+++ src/components/events/screens/EventsList/EventsList.tsx
@@ -1,5 +1,28 @@
-export default function EventsList() {
+import type { Signal } from "@preact/signals";
+import type { AppState } from "../../state/state";
+import type { MeetupItem } from "../../../../types/types";
 
-  
-  return <div>List here</div>
+type EventsListProps = {
+  state: Signal<AppState>;
+};
+export default function EventsList({ state }: EventsListProps) {
+  const { data = null } = state.value.meetups;
+  if (!data) {
+    return <div>Loading list...</div>;
+  }
+
+  return (
+    <div class="my-6">
+      {data.map((item) => (
+        <ListItem item={item} key={item.meetupId} />
+      ))}
+    </div>
+  );
+}
+
+type ListItemProps = {
+  item: MeetupItem;
+};
+function ListItem({ item }: ListItemProps) {
+  return <div class="my-6">{item.title}</div>;
 }
