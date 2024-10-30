e446781c1c0b1947b226cbd41b62cbc96ad168a8
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2024-10-30 12:32:06 +0100

    Tidy up form

--- src/components/events/screens/AttendModal/parts/FormStepper.tsx
+++ src/components/events/screens/AttendModal/parts/FormStepper.tsx
@@ -105,8 +105,45 @@ export default function FormStepper() {
 
   return (
     <div>
-      {/* STEP 0 */}
       <Step
+        currentStep={0}
+        stepIndex={-1}
+        stepTitle="Attendance"
+        stepDescription="Please let us know if you can make it."
+        fieldsetTitle="Are you coming?"
+      >
+        <div class="space-y-6">
+          {responseOptions.map((option) => {
+            const optionId = `option_${option}`;
+            return (
+              <div
+                key={optionId}
+                class={cn(
+                  "flex items-center gap-x-3"
+                  // "border-b border-b-slate-200 py-8 last:border-b-0"
+                )}
+              >
+                <input
+                  name="rsvpOption"
+                  type="radio"
+                  value={option}
+                  id={optionId}
+                  onClick={handleRadioClick}
+                  class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600 cursor-pointer"
+                />
+                <label
+                  htmlFor={optionId}
+                  class="block text-sm/6 font-medium text-gray-900 cursor-pointer"
+                >
+                  {getRsvpOptionLabel(option)}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </Step>
+      {/* STEP 0 */}
+      {/* <Step
         stepTitle="Are you coming?"
         stepIndex={0}
         currentStep={localFormState.value.currentStep}
@@ -142,15 +179,115 @@ export default function FormStepper() {
             })}
           </div>
         </div>
-      </Step>
+      </Step> */}
 
       {/* STEP 1 */}
       <Step
-        stepTitle="Your info"
+        stepTitle="Personal Information"
+        stepDescription="Please tell us who you are."
         stepIndex={1}
         currentStep={localFormState.value.currentStep}
       >
-        <div class="rounded-md overflow-hidden border border-slate-200 bg-slate-50">
+        <div class="space-y-6">
+          <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="sm:col-span-3">
+              <label
+                for="first-name"
+                class="block text-sm/6 font-medium text-gray-900"
+              >
+                First name
+              </label>
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autocomplete="given-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label
+                for="last-name"
+                class="block text-sm/6 font-medium text-gray-900"
+              >
+                Last name (or initial)
+              </label>
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autocomplete="family-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            {/* <div class="sm:col-span-4">
+              <label
+                for="email"
+                class="block text-sm/6 font-medium text-gray-900"
+              >
+                Email address
+              </label>
+              <div class="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div> */}
+
+            {/* <div class="sm:col-span-3">
+              <label
+                for="country"
+                class="block text-sm/6 font-medium text-gray-900"
+              >
+                Country
+              </label>
+              <div class="mt-2">
+                <select
+                  id="country"
+                  name="country"
+                  autocomplete="country-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm/6"
+                >
+                  <option>United States</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                </select>
+              </div>
+            </div> */}
+
+            {/* <div class="col-span-full">
+              <label
+                for="street-address"
+                class="block text-sm/6 font-medium text-gray-900"
+              >
+                Street address
+              </label>
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="street-address"
+                  id="street-address"
+                  autocomplete="street-address"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div> */}
+
+            
+
+            
+          </div>
+
           <div class="flex flex-col">
             <div class="py-4 px-6 flex flex-col gap-1">
               <label
@@ -214,17 +351,14 @@ export default function FormStepper() {
         </div>
       </Step>
 
-      <div class="border-b border-gray-900/10 pb-12">
+      {/* <div class="border-b border-gray-900/10 pb-6 my-6">
         <h2 class="text-base/7 font-semibold text-gray-900">
           Personal Information
         </h2>
-        <p class="mt-1 text-sm/6 text-gray-600">
-          Please tell us who you are.
-        </p>
+        <p class="mt-1 text-sm/6 text-gray-600">Please tell us who you are.</p>
 
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-3">
-
             <label
               for="first-name"
               class="block text-sm/6 font-medium text-gray-900"
@@ -368,131 +502,7 @@ export default function FormStepper() {
             </div>
           </div>
         </div>
-      </div>
-
-      <div class="border-b border-gray-900/10 pb-12">
-        <h2 class="text-base/7 font-semibold text-gray-900">Notifications</h2>
-        <p class="mt-1 text-sm/6 text-gray-600">
-          We'll always let you know about important changes, but you pick what
-          else you want to hear about.
-        </p>
-
-        <div class="mt-10 space-y-10">
-          <fieldset>
-            <legend class="text-sm/6 font-semibold text-gray-900">
-              By Email
-            </legend>
-            <div class="mt-6 space-y-6">
-              <div class="relative flex gap-x-3">
-                <div class="flex h-6 items-center">
-                  <input
-                    id="comments"
-                    name="comments"
-                    type="checkbox"
-                    class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                  />
-                </div>
-                <div class="text-sm/6">
-                  <label for="comments" class="font-medium text-gray-900">
-                    Comments
-                  </label>
-                  <p class="text-gray-500">
-                    Get notified when someones posts a comment on a posting.
-                  </p>
-                </div>
-              </div>
-              <div class="relative flex gap-x-3">
-                <div class="flex h-6 items-center">
-                  <input
-                    id="candidates"
-                    name="candidates"
-                    type="checkbox"
-                    class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                  />
-                </div>
-                <div class="text-sm/6">
-                  <label for="candidates" class="font-medium text-gray-900">
-                    Candidates
-                  </label>
-                  <p class="text-gray-500">
-                    Get notified when a candidate applies for a job.
-                  </p>
-                </div>
-              </div>
-              <div class="relative flex gap-x-3">
-                <div class="flex h-6 items-center">
-                  <input
-                    id="offers"
-                    name="offers"
-                    type="checkbox"
-                    class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                  />
-                </div>
-                <div class="text-sm/6">
-                  <label for="offers" class="font-medium text-gray-900">
-                    Offers
-                  </label>
-                  <p class="text-gray-500">
-                    Get notified when a candidate accepts or rejects an offer.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </fieldset>
-          <fieldset>
-            <legend class="text-sm/6 font-semibold text-gray-900">
-              Push Notifications
-            </legend>
-            <p class="mt-1 text-sm/6 text-gray-600">
-              These are delivered via SMS to your mobile phone.
-            </p>
-            <div class="mt-6 space-y-6">
-              <div class="flex items-center gap-x-3">
-                <input
-                  id="push-everything"
-                  name="push-notifications"
-                  type="radio"
-                  class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                />
-                <label
-                  for="push-everything"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
-                  Everything
-                </label>
-              </div>
-              <div class="flex items-center gap-x-3">
-                <input
-                  id="push-email"
-                  name="push-notifications"
-                  type="radio"
-                  class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                />
-                <label
-                  for="push-email"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
-                  Same as email
-                </label>
-              </div>
-              <div class="flex items-center gap-x-3">
-                <input
-                  id="push-nothing"
-                  name="push-notifications"
-                  type="radio"
-                  class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                />
-                <label
-                  for="push-nothing"
-                  class="block text-sm/6 font-medium text-gray-900"
-                >
-                  No push notifications
-                </label>
-              </div>
-            </div>
-          </fieldset>
-        </div>
-      </div>
+      </div> */}
 
       <div>
         {localFormState.value.rsvps.length > 0 && (
@@ -513,7 +523,6 @@ export default function FormStepper() {
           </div>
         )}
       </div>
-      
     </div>
   );
 }
@@ -521,21 +530,37 @@ export default function FormStepper() {
 type StepProps = {
   children: ComponentChildren;
   stepTitle: string;
+  stepDescription?: string;
+  fieldsetTitle?: string;
   stepIndex: number;
   currentStep: number;
 };
-function Step({ stepTitle, stepIndex, currentStep, children }: StepProps) {
+function Step({
+  stepTitle,
+  stepDescription,
+  fieldsetTitle,
+  stepIndex,
+  currentStep,
+  children,
+}: StepProps) {
   return (
-    <div class="my-8">
-      <details
-        open
-        // open={stepIndex === currentStep}
-      >
-        <summary class="text-xl mb-2 font-semibold tracking-tight">
-          {stepTitle}
-        </summary>
-        <div>{children}</div>
-      </details>
+    <div class="border-b border-gray-900/10 pb-6 my-6">
+      {stepTitle && (
+        <h2 class="text-xl/7 font-semibold text-gray-900">{stepTitle}</h2>
+      )}
+      {stepDescription && (
+        <p class="mt-1 mb-4 text-sm/6 text-gray-600">{stepDescription}</p>
+      )}
+      <div class="p-4 py-6 bg-slate-100 border border-slate-200 rounded-md space-y-10">
+        <fieldset class="text-xl mb-2 font-semibold tracking-tight">
+          {fieldsetTitle && (
+            <legend class="text-sm/6 font-normal text-gray-600 mb-4">
+              {fieldsetTitle}
+            </legend>
+          )}
+          {children}
+        </fieldset>
+      </div>
     </div>
   );
 }
