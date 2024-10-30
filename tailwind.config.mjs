77c5c105c404b94be44eec2c82619ff0e2b65941
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2024-10-30 11:59:06 +0100

    Adding TW forms plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,13 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
-		},
-	},
-	plugins: [require("@tailwindcss/typography")],
-}
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
