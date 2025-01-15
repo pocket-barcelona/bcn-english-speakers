e755eedb8666861ee625cdd6daa8f9abdff746e5
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2025-01-15 17:42:46 +0100

    Event guests

--- src/components/events/components/EventModal/parts/EventGuests.tsx
+++ src/components/events/components/EventModal/parts/EventGuests.tsx
@@ -1,6 +1,11 @@
 import { RiAccountCircleFill } from "../../Icons/Icons";
-import { MeetupRsvpAttendanceStatusEnum, type MeetupGroupItem, type MeetupItem } from '../../../types/types';
-import { getGuestList } from '../../../utils/utils';
+import {
+  MeetupRsvpAttendanceStatusEnum,
+  type MeetupGroupItem,
+  type MeetupItem,
+} from "../../../types/types";
+import { getGuestList } from "../../../utils/utils";
+import { useCallback } from "preact/hooks";
 
 type EventGuestsProps = {
   event: MeetupItem;
@@ -8,17 +13,44 @@ type EventGuestsProps = {
 };
 export default function EventGuests({ event, group }: EventGuestsProps) {
   const { rsvps } = event;
+  // const confirmedGuests = useCallback(
+  //   () => getGuestList(rsvps, MeetupRsvpAttendanceStatusEnum.Coming),
+  //   [rsvps]
+  // );
+  // const unconfirmedGuests = useCallback(
+  //   () => getGuestList(rsvps, MeetupRsvpAttendanceStatusEnum.Cannot),
+  //   [rsvps]
+  // );
   const confirmedGuests = getGuestList(rsvps, MeetupRsvpAttendanceStatusEnum.Coming);
+  const unconfirmedGuests = getGuestList(rsvps, MeetupRsvpAttendanceStatusEnum.Cannot);
 
-  // TODO...
+  return (
+    <>
+      <GuestBlock
+        title="Confirmed guests"
+        numberOfGuests={confirmedGuests.length.toString()}
+      />
+      <hr />
+      <GuestBlock
+        title="Un-confirmed guests"
+        numberOfGuests={unconfirmedGuests.length.toString()}
+      />
+    </>
+  );
+}
+
+type GuestBlockProps = {
+  title: string;
+  numberOfGuests: string;
+};
+function GuestBlock({ title, numberOfGuests }: GuestBlockProps) {
   return (
     <div class="flex flex-col gap-2">
-      <h2 class="text-base tracking-tight font-semibold">Guests</h2>
+      <h2 class="text-base tracking-tight font-semibold">{title}</h2>
       <div class="flex flex-row items-start gap-2">
         <RiAccountCircleFill width={24} height={24} />
-
         <div class="flex flex-row flex-wrap relative flex-grow">
-          {confirmedGuests.length} guest/s
+          {numberOfGuests} guest/s
         </div>
       </div>
     </div>
