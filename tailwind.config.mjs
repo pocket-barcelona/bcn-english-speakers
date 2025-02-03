5ce4ccfe1e1e59872c4968e976824f4502a0c195
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2025-02-03 23:23:50 +0100

    Squashed commit of the following:
    commit 6dcc954651b70bcbdd56f05328eb755f2e747582
    Author: Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
    Date:   Mon Feb 3 23:19:10 2025 +0100
    
        Llum BCN
    
    commit f6b7b5ebcf2647e647340567165bfd10c473d903
    Author: Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
    Date:   Mon Feb 3 22:35:35 2025 +0100
    
        Tidy up layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,17 @@ export default {
 			fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
+			aspectRatio: {
+				"4/3": "4 / 3",
+				"3/4": "3 / 4",
+				"16/9": "16 / 9",
+				"9/16": "9 / 16",
+				"21/9": "21 / 9",
+				"9/21": "9 / 21",
+				"1/1": "1 / 1",
+				"3/2": "3 / 2",
+				"2/3": "2 / 3",
+			},
 		},
 	},
 	plugins: [require("@tailwindcss/typography")],
