806567775cfea34e674fdfa0c7ce3e8daa219332
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2025-03-09 18:38:26 +0100

    Vitest and coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,18 @@ import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
   test: {
-    // Vitest configuration options
+    coverage: {
+      provider: "v8",
+      enabled: true,
+      all: false,
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
+      include: ["src/**/*.{ts,tsx}"],
+    },
+    exclude: ["node_modules/**"],
   },
 });
